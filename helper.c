252064228b04175fd5c42fcecd769c89c6829428
Benjamin Kauffmann <benjaminkauffmann@hotmail.fr>
2020-11-21 15:22:22 +0100

    first commit

---
+++ helper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int knn_supervised(double[] train_data) {    
+
+    return 0;
+}
+
+
+void k_means(double[][] image) {    
+    return 0;
+}
+
+void squared_error_partitioning(double[][] image) {    
+
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static int main(int argc, char * argv[] ) {
+    
+    return 0 ;
+}
